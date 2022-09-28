3f750f75e71a60cc80f9865adbc8779e7fdcf3f6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-28 09:38:03 +0430

    Add project images

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import _ from 'lodash';
 import './styles.css';
+import cross from './icon-cross.svg';
+import moon from './icon-moon.svg';
+import sun from './icon-sun.svg';
+
+const crossIcon = document.querySelectorAll('.cross-icon');
+document.querySelector('.themeChanger').src = moon;
+crossIcon.forEach((element) => {
+  element.src = cross;
+});
