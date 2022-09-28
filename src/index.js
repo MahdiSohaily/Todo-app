ab2700b9edc8fd87a8af3bae163f9791f9c0c487
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-28 10:27:52 +0430

    Append list items to container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
 // project style file
 import './styles.css';
-
+// import project modules
 import mainEvent from './modules/mainEvents';
+import appendTOdo from './modules/Todo';
 
+const container = document.querySelector('.todo-list');
+container.innerHTML = appendTOdo();
 mainEvent();
--- src/modules/mainEvents.js
+++ src/modules/mainEvents.js
@@ -3,19 +3,19 @@ import cross from '../images/icon-cross.svg';
 import moon from '../images/icon-moon.svg';
 import sun from '../images/icon-sun.svg';
 
-export default function mainEvent () {
+export default function mainEvent() {
   const crossIcon = document.querySelectorAll('.cross-icon');
   document.querySelector('.themeChanger').src = moon;
   crossIcon.forEach((element) => {
     element.src = cross;
   });
 
-  const todoinput = document.querySelector('.todo-input');
-  todoinput.addEventListener('focus', (e) => {
+  const todoInput = document.querySelector('.todo-input');
+  todoInput.addEventListener('focus', (e) => {
     e.target.placeholder = 'Currently typing...';
   });
 
-  todoinput.addEventListener('blur', (e) => {
+  todoInput.addEventListener('blur', (e) => {
     e.target.placeholder = 'Create a new todo...';
   });
-};
+}
