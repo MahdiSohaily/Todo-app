b4efcd32b4576fa4cfd1350c7734233fb87d6f97
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-28 09:41:29 +0430

    Update files path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import './styles.css';
-import cross from './icon-cross.svg';
-import moon from './icon-moon.svg';
-import sun from './icon-sun.svg';
+import cross from './images/icon-cross.svg';
+import moon from './images/icon-moon.svg';
+import sun from './images/icon-sun.svg';
 
 const crossIcon = document.querySelectorAll('.cross-icon');
 document.querySelector('.themeChanger').src = moon;
