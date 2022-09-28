1be08e830254dab7babea3172fa184643ea3769f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-28 21:44:28 +0430

    Add comment to describe the code

--- src/modules/mainEvents.js
+++ src/modules/mainEvents.js
@@ -4,18 +4,23 @@ import moon from '../images/icon-moon.svg';
 // import sun from '../images/icon-sun.svg';
 
 export default function mainEvents() {
+  // setting src attribute od every todo item
   const crossIcon = document.querySelectorAll('.cross-icon');
-  document.querySelector('.themeChanger').src = moon;
   crossIcon.forEach((element) => {
     element.src = cross;
   });
 
+  // changing todo input element's placeholder value on focus
   const todoInput = document.querySelector('.todo-input');
   todoInput.addEventListener('focus', (e) => {
     e.target.placeholder = 'Currently typing...';
   });
 
+  // changing todo input element's placeholder value on blur
   todoInput.addEventListener('blur', (e) => {
     e.target.placeholder = 'Create a new todo...';
   });
+
+
+  document.querySelector('.themeChanger').src = moon;
 }
