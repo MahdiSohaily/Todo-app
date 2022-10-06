2ee95c7d625d6b54c778284365ab00e9a511dcc9
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-06 21:13:26 +0430

    Update test implementation

--- src/tests/Todo.test.js
+++ src/tests/Todo.test.js
@@ -4,43 +4,40 @@
 import Todo from '../modules/Todo';
 
 // Script to test the
-escribe('Addition test', () => {
+describe('Addition test', () => {
   // Test One
   test('Adding new TODO item to local storage', () => {
     const obj = new Todo('Item One');
+    obj.addTodo();
     const data = localStorage.getItem('todos')
       ? JSON.parse(localStorage.getItem('todos'))
       : [];
-    obj.addTodo();
-    console.log(obj);
-    const len = data.length;
-    console.log(data);
-    expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
+    const current = JSON.stringify(data[data.length - 1]);
+
+    expect(JSON.stringify(obj)).toEqual(current);
   });
 
   // Test Two
   test('Adding new TODO item to local storage', () => {
     const obj = new Todo('Item Two');
+    obj.addTodo();
     const data = localStorage.getItem('todos')
       ? JSON.parse(localStorage.getItem('todos'))
       : [];
-    obj.addTodo();
-    console.log(obj);
-    const len = data.length;
-    console.log(data);
-    expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
+    const current = JSON.stringify(data[data.length - 1]);
+
+    expect(JSON.stringify(obj)).toEqual(current);
   });
 
   //   Test Three
   test('Adding new TODO item to local storage', () => {
     const obj = new Todo('Item Three');
+    obj.addTodo();
     const data = localStorage.getItem('todos')
       ? JSON.parse(localStorage.getItem('todos'))
       : [];
-    obj.addTodo();
-    console.log(obj);
-    const len = data.length;
-    console.log(data);
-    expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
+    const current = JSON.stringify(data[data.length - 1]);
+
+    expect(JSON.stringify(obj)).toEqual(current);
   });
 });
