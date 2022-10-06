79e6240efd1a7b84aca6330d630e67776bcc9fd2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-06 20:59:40 +0430

    Add test one for add todo

--- src/tests/Todo.test.js
+++ src/tests/Todo.test.js
@@ -3,3 +3,20 @@
  */
 import Todo from '../modules/Todo';
 
+
+// Script to test the
+escribe('Addition test', () => {
+    // Test One
+    test('Adding new TODO item to local storage', () => {
+      const obj = new Todo('Item One');
+      const data = localStorage.getItem('todos')
+        ? JSON.parse(localStorage.getItem('todos'))
+        : [];
+      obj.addTodo();
+      console.log(obj);
+      const len = data.length;
+      console.log(data);
+      expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
+    });
+
+  });
