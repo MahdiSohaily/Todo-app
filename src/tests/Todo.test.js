91e1f03176565fb52c5782dcc10e16567a8db575
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-06 21:00:23 +0430

    Add test Three for add todo

--- src/tests/Todo.test.js
+++ src/tests/Todo.test.js
@@ -3,32 +3,44 @@
  */
 import Todo from '../modules/Todo';
 
-
 // Script to test the
 escribe('Addition test', () => {
-    // Test One
-    test('Adding new TODO item to local storage', () => {
-      const obj = new Todo('Item One');
-      const data = localStorage.getItem('todos')
-        ? JSON.parse(localStorage.getItem('todos'))
-        : [];
-      obj.addTodo();
-      console.log(obj);
-      const len = data.length;
-      console.log(data);
-      expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
-    });
-  
-    // Test Two
-    test('Adding new TODO item to local storage', () => {
-      const obj = new Todo('Item One');
-      const data = localStorage.getItem('todos')
-        ? JSON.parse(localStorage.getItem('todos'))
-        : [];
-      obj.addTodo();
-      console.log(obj);
-      const len = data.length;
-      console.log(data);
-      expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
-    });
+  // Test One
+  test('Adding new TODO item to local storage', () => {
+    const obj = new Todo('Item One');
+    const data = localStorage.getItem('todos')
+      ? JSON.parse(localStorage.getItem('todos'))
+      : [];
+    obj.addTodo();
+    console.log(obj);
+    const len = data.length;
+    console.log(data);
+    expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
   });
+
+  // Test Two
+  test('Adding new TODO item to local storage', () => {
+    const obj = new Todo('Item One');
+    const data = localStorage.getItem('todos')
+      ? JSON.parse(localStorage.getItem('todos'))
+      : [];
+    obj.addTodo();
+    console.log(obj);
+    const len = data.length;
+    console.log(data);
+    expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
+  });
+
+  //   Test Three
+  test('Adding new TODO item to local storage', () => {
+    const obj = new Todo('Item One');
+    const data = localStorage.getItem('todos')
+      ? JSON.parse(localStorage.getItem('todos'))
+      : [];
+    obj.addTodo();
+    console.log(obj);
+    const len = data.length;
+    console.log(data);
+    expect(JSON.parse(localStorage.getItem('todos')).length).toBe(len + 1);
+  });
+});
